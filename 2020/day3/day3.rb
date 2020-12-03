d94128554ebc1c36f7415c77313a4a7f1f134f70
Utkarsh Gupta <utkarsh@debian.org>
2020-12-03 18:45:42 +0530

    Add day3, part2
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- 2020/day3/day3.rb
+++ 2020/day3/day3.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 
 c = 0
-mr3d1 = 0
-hr3d1 = 0
+mr1d1 = mr3d1 = mr5d1 = mr7d1 = mr1d2 = 0
+hr1d1 = hr3d1 = hr5d1 = hr7d1 = hr1d2 = 0
 
 def count_hash(i, marker)
   if i[marker] == "#"
@@ -17,9 +17,28 @@ ARGF.each_line do |i|
   next if c == 1
   i = i.chomp.to_s * c
 
+  # for right 1, down 1
+  r1d1 = count_hash(i, mr1d1 += 1)
+  hr1d1 += 1 if r1d1 == "+"
+
   # for right 3, down 1
   r3d1 = count_hash(i, mr3d1 += 3)
   hr3d1 += 1 if r3d1 == "+"
+
+  # for right 5, down 1
+  r5d1 = count_hash(i, mr5d1 += 5)
+  hr5d1 += 1 if r5d1 == "+"
+
+  # for right 7, down 1
+  r7d1 = count_hash(i, mr7d1 += 7)
+  hr7d1 += 1 if r7d1 == "+"
+
+  # for right 1, down 1
+  if c > 2 and c.odd?
+    r1d2 = count_hash(i, mr1d2 += 1)
+    hr1d2 += 1 if r1d2 == "+"
+  end
 end
 
 puts hr3d1
+puts hr1d1 * hr3d1 * hr5d1 * hr7d1 * hr1d2
