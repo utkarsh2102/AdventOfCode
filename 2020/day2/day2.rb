623561ac7658f57f92783dbcbeeaef9fdadc0bf7
Utkarsh Gupta <utkarsh@debian.org>
2020-12-03 18:59:47 +0530

    RuboCop day2
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- 2020/day2/day2.rb
+++ 2020/day2/day2.rb
@@ -5,24 +5,20 @@ counter_part2 = 0
 
 ARGF.each_line do |i|
   str = i.chomp.to_s
-  low = str.partition("-")[0].to_i
-  high = str.partition("-")[2].partition(" ")[0].to_i
-  key = str.partition(":")[0].partition(" ")[2].to_s
-  passwd = str.partition(": ")[2].to_s
+  low = str.partition('-')[0].to_i
+  high = str.partition('-')[2].partition(' ')[0].to_i
+  key = str.partition(':')[0].partition(' ')[2].to_s
+  passwd = str.partition(': ')[2].to_s
 
   # Part 1
   count = passwd.count(key)
-  if count >= low and count <= high
-    counter_part1 += 1
-  end
+  counter_part1 += 1 if (count >= low) && (count <= high)
 
   # Part 2
   flag = 0
-  flag += 1 if passwd[low-1] == key
-  flag += 1 if passwd[high-1] == key
-  if flag == 1
-    counter_part2 += 1
-  end   
+  flag += 1 if passwd[low - 1] == key
+  flag += 1 if passwd[high - 1] == key
+  counter_part2 += 1 if flag == 1
 end
 
 puts counter_part1
