f3f801e0197701aeed95186928aa047cf350bba5
Utkarsh Gupta <utkarsh@debian.org>
2020-12-04 20:26:01 +0530

    Add day4, part2
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- 2020/day4/day4.rb
+++ 2020/day4/day4.rb
@@ -1,7 +1,45 @@
 #!/usr/bin/ruby
 
 arr = []
-c = valid = 0
+c = valid = valid_res = 0
+
+def part_two(str)
+  byr = iyr = eyr = hgt = hcl = ecl = pid = 0
+
+  byr_n = str.scan(/byr:\d+/)[0].split(":")[1].to_i if !str.scan(/byr:\d+/).empty?
+  byr = 1 if byr_n >= 1920 and byr_n <= 2002
+
+  iyr_n = str.scan(/iyr:\d+/)[0].split(":")[1].to_i if !str.scan(/iyr:\d+/).empty?
+  iyr = 1 if iyr_n >= 2010 and iyr_n <= 2020
+
+  eyr_n = str.scan(/eyr:\d+/)[0].split(":")[1].to_i if !str.scan(/eyr:\d+/).empty?
+  eyr = 1 if eyr_n >= 2020 and eyr_n <= 2030
+
+  hgt_s = str.scan(/hgt:\w+/)[0].split(":")[1].scan(/[A-z]/).join("")
+  hgt_n = str.scan(/hgt:\w+/)[0].split(":")[1].scan(/\d+/)[0].to_i
+  if hgt_s == "cm" and hgt_n >= 150 and hgt_n <= 193
+    hgt = 1
+  elsif hgt_s == "in" and hgt_n >= 59 and hgt_n <= 76
+    hgt = 1
+  else
+    hgt = 0
+  end
+
+  hcl = 1 if !str.scan(/hcl:#\w+/).empty? and str.scan(/hcl:#\w+/)[0].split(":#")[1].to_s.size == 6
+
+  ecl_s = str.scan(/ecl:\w+/)[0].split(":")[1].to_s if !str.scan(/ecl:\w+/).empty?
+  ecl = 1 if ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include?(ecl_s)
+
+  pid = 1 if !str.scan(/pid:\d+/).empty? and str.scan(/pid:\d+/)[0].split(":")[1].to_s.size == 9
+
+  # debugging
+  # puts "byr=#{byr} iyr=#{iyr} eyr=#{eyr} hgt=#{hgt} hcl=#{hcl} elc=#{ecl} pid=#{pid}"
+
+  # final check to see all conditions match
+  if [byr, iyr, eyr, hgt, hcl, ecl, pid].all?{ |i| i == 1 }
+    "YES"
+  end
+end
 
 ARGF.each_line do |str|
   if str.chomp == ""
@@ -20,11 +58,20 @@ end
   keys = str.scan(/byr:|iyr:|eyr:|hgt:|hcl:|ecl:|pid:|cid:/)
   if keys.size == 8
     valid += 1
+    check_two = part_two(str)
+    if check_two == "YES"
+      valid_res += 1
+    end
   elsif keys.size == 7 and !keys.include?("cid:")
     valid += 1
+    check_two = part_two(str)
+    if check_two == "YES"
+      valid_res += 1
+    end
   else
     next
   end
 end
 
 puts valid
+puts valid_res
