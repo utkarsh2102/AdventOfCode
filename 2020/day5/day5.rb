8a064c0f0e94a2ddedd01dc2c5fd890e09f84ea3
Utkarsh Gupta <utkarsh@debian.org>
2020-12-05 12:16:17 +0530

    RuboCop day5
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- 2020/day5/day5.rb
+++ 2020/day5/day5.rb
@@ -4,10 +4,10 @@ highest = 0
 seats = []
 
 def halfify(arr, ltr)
-  if ltr == "F" or ltr == "L"
-    return arr[0, arr.size/2]
+  if (ltr == 'F') || (ltr == 'L')
+    arr[0, arr.size / 2]
   else
-    return arr[(arr.size+1)/2..arr[-1]]
+    arr[(arr.size + 1) / 2..arr[-1]]
   end
 end
 
@@ -16,13 +16,14 @@ ARGF.each_line do |str|
   carr = (0..7).to_a
   rows = str[0..6].to_s
   coul = str[7..9].to_s
-  0.upto rows.size-1 do |i|
+
+  0.upto rows.size - 1 do |i|
     ltr = rows[i]
     rarr = halfify(rarr, ltr)
   end
   row = rarr[0]
 
-  0.upto coul.size-1 do |j|
+  0.upto coul.size - 1 do |j|
     ltr = coul[j]
     carr = halfify(carr, ltr)
   end
@@ -30,9 +31,7 @@ ARGF.each_line do |str|
 
   sid = row * 8 + cou
   seats << sid
-  if sid >= highest
-    highest = sid
-  end
+  highest = sid if sid >= highest
 end
 
 seats.sort!
