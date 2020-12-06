84f8b611db835f53859b93e2ba5a1d3f710a1259
Utkarsh Gupta <utkarsh@debian.org>
2020-12-06 20:27:21 +0530

    Add day6, part2
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- 2020/day6/day6.rb
+++ 2020/day6/day6.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/ruby
 
+groups = File.read('input').split("\n\n")
+
+# Another very nice way of taking inputs
+# groups = File
+#   .read('tmp')
+#   .split("\n\n")
+#   .map { |group| group.lines.map(&:chomp) }
+
+# Part 1
 arr = []
 count = 0
 
@@ -17,3 +26,7 @@ ARGF.each_line do |str|
 end
 
 puts count + arr.size
+
+
+# Part 2
+p groups.sum { |group| group.split.map(&:chars).reduce(&:&).length }
