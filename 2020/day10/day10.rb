3beebfe2e44689f234c930cfdaf6f6e8b40df286
Utkarsh Gupta <utkarsh@debian.org>
2020-12-20 04:16:57 +0530

    RuboCop day10
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- 2020/day10/day10.rb
+++ 2020/day10/day10.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-arr = File.read("input").split("\n").map(&:to_i)
+arr = File.read('input').split("\n").map(&:to_i)
 arr.sort!
 
 arr.unshift(0)
@@ -9,8 +9,8 @@ arr << arr.last + 3
 # Part 1
 j1 = j3 = 0
 
-0.upto arr.size-2 do |i|
-  if arr[i+1] - arr[i] == 1
+0.upto arr.size - 2 do |i|
+  if arr[i + 1] - arr[i] == 1
     j1 += 1
   else
     j3 += 1
@@ -25,7 +25,7 @@ pos[0] = 1
 
 arr.each do |i|
   3.times do |j|
-    pos[i] += pos[i-j-1]
+    pos[i] += pos[i - j - 1]
   end
 end
 
