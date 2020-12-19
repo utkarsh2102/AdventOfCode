8daaaafb530c8a9c0047b92e16fcb235ccf09f67
Utkarsh Gupta <utkarsh@debian.org>
2020-12-19 20:10:57 +0530

    RuboCop day8
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- 2020/day8/day8.rb
+++ 2020/day8/day8.rb
@@ -1,31 +1,31 @@
 #!/usr/bin/ruby
 
 arr = File.read('input')
-             .split("\n")
+          .split("\n")
 
 # Part 1
 acc = i = 0
 visited = []
 
 loop do
-  if arr[i].nil? or arr[i].chomp == "" or visited.include?(i)
+  if arr[i].nil? || (arr[i].chomp == '') || visited.include?(i)
     break
   else
     visited << i
     ins = arr[i][0..2]
-    if ins == "nop"
+    if ins == 'nop'
       i += 1
     else
       op = arr[i][4]
       val = arr[i].chomp.split(op)[1].to_i
 
-      if ins == "acc"
-        acc += val if op == "+"
-        acc -= val if op == "-"
+      if ins == 'acc'
+        acc += val if op == '+'
+        acc -= val if op == '-'
         i += 1
       else
-        i += val if op == "+"
-        i -= val if op == "-"
+        i += val if op == '+'
+        i -= val if op == '-'
       end
     end
   end
@@ -33,49 +33,46 @@ end
 
 p "Part 1: #{acc}"
 
-
 # Part 2
-0.upto arr.size-1 do |j|
-  if arr[j][0..2] == "acc"
+0.upto arr.size - 1 do |j|
+  if arr[j][0..2] == 'acc'
     next
   else
     tmp = arr.map(&:clone)
 
-    if tmp[j][0..2] == "jmp"
-      tmp[j].gsub!("jmp", "nop")
+    if tmp[j][0..2] == 'jmp'
+      tmp[j].gsub!('jmp', 'nop')
     else
-      tmp[j].gsub!("nop", "jmp")
+      tmp[j].gsub!('nop', 'jmp')
     end
 
     acc = i = 0
     visited = []
 
     loop do
-      if tmp[i].nil? or tmp[i].chomp == "" or visited.include?(i)
+      if tmp[i].nil? || (tmp[i].chomp == '') || visited.include?(i)
         break
       else
         visited << i
         ins = tmp[i][0..2]
-        if ins == "nop"
+        if ins == 'nop'
           i += 1
         else
           op = tmp[i][4]
           val = tmp[i].chomp.split(op)[1].to_i
 
-          if ins == "acc"
-            acc += val if op == "+"
-            acc -= val if op == "-"
+          if ins == 'acc'
+            acc += val if op == '+'
+            acc -= val if op == '-'
             i += 1
           else
-            i += val if op == "+"
-            i -= val if op == "-"
+            i += val if op == '+'
+            i -= val if op == '-'
           end
         end
       end
     end
 
-    if visited.include?(tmp.size-1)
-      p "Part 2: #{acc}"
-    end
+    p "Part 2: #{acc}" if visited.include?(tmp.size - 1)
   end
 end
